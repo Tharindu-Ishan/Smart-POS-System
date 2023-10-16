45668162030ed0f6c0b2be54af9f137d7a644932
Tharindu Ishan <ishanthari96@gmail.com>
2023-10-16 14:16:54 +0530

    Finalize the Project

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Smart POS
 Revolutionizing retail with intuitive, all-in-one point of sale software. Seamlessly manage transactions, inventory, and customer relations with cutting-edge efficiency. Designed for the modern business. Elevate your sales experience.
 
+![img.png](img.png)
 ### License
-Copyright &copy; 2023 DEP-11. All Rights Reserved.
+Copyright &copy; 2023 Tharindu Ishan. All Rights Reserved.
--- src/main/java/lk/ijse/dep11/pos/db/OrderDataAccess.java
+++ src/main/java/lk/ijse/dep11/pos/db/OrderDataAccess.java
@@ -1,8 +1,11 @@
 package lk.ijse.dep11.pos.db;
 
+import lk.ijse.dep11.pos.tm.Order;
 import lk.ijse.dep11.pos.tm.OrderItem;
 
+import java.math.BigDecimal;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderDataAccess {
@@ -13,6 +16,7 @@ public class OrderDataAccess {
     private static final PreparedStatement STM_INSERT_ORDER;
     private static final PreparedStatement STM_INSERT_ORDER_ITEM;
     private static final PreparedStatement STM_UPDATE_STOCK;
+    private static final PreparedStatement STM_FIND;
 
 
     static {
@@ -29,6 +33,16 @@ public class OrderDataAccess {
                     ("INSERT INTO order_item (order_id, item_code, qty, unit_price) VALUES (?,?,?,?)");
             STM_UPDATE_STOCK = connection.prepareStatement
                     ("UPDATE item SET qty = qty - ? WHERE code = ?");
+            STM_FIND=connection.prepareStatement("SELECT o.*, c.name, CAST(order_total.total AS DECIMAL(8,2))\n" +
+                    "FROM \"order\" AS o\n" +
+                    "         INNER JOIN customer AS c ON o.customer_id = c.id\n" +
+                    "        INNER JOIN\n" +
+                    "(SELECT o.id, SUM(qty * unit_price) AS total\n" +
+                    "FROM \"order\" AS o\n" +
+                    "         INNER JOIN order_item AS oi ON oi.order_id = o.id GROUP BY o.id) AS order_total\n" +
+                    "ON o.id = order_total.id\n" +
+                    "WHERE o.id LIKE ? OR CAST(o.date AS VARCHAR(20)) LIKE ? OR o.customer_id LIKE ? OR c.name LIKE ? " +
+                    "ORDER BY o.id");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -76,6 +90,21 @@ public class OrderDataAccess {
         }finally{
             SingleConnectionDataSource.getInstance().getConnection().setAutoCommit(true);
         }
+    }
+    public static List<Order> findOrders(String query) throws SQLException{
+        for(int i = 1; i <= 4; i++)
+            STM_FIND.setString(i, "%".concat(query).concat("%"));
+        ResultSet rst = STM_FIND.executeQuery();
+        List<Order> orderList = new ArrayList<>();
+        while (rst.next()){
+            String orderId = rst.getString("id");
+            Date orderDate = rst.getDate("date");
+            String customerId = rst.getString("customer_id");
+            String customerName = rst.getString("name");
+            BigDecimal orderTotal = rst.getBigDecimal("total");
+            orderList.add(new Order(orderId, orderDate.toString(), customerId, customerName, orderTotal));
+        }
+        return orderList;
     }
 
 }
